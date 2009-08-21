ab6da82650a3196d709b07b65cef0d45523eda32
luelher <luelher@43d773e8-0283-11de-bc25-5f29055a2a2d>
2009-08-21 01:30:42 +0000

    Version inicial de creditum para produccion.

--- creditum/web/index.php
+++ creditum/web/index.php
@@ -3,5 +3,5 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('creditum', 'dev', false);
+$configuration = ProjectConfiguration::getApplicationConfiguration('creditum', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
